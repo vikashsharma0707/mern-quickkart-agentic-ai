6286f283d06643eaa9ec84d226bb2899f5e58012
vikash sharma <vikashtiwari9893@gmail.com>
2026-06-19 15:54:03 +0530

    code

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,21 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// export default defineConfig({
+//   plugins: [react()],
+//   server: { port: 5173, proxy: { "/api": "https://mern-quickkart-agentic-ai-1.onrender.com", "/uploads": "https://mern-quickkart-agentic-ai-1.onrender.com" } },
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, proxy: { "/api": "https://mern-quickkart-agentic-ai-1.onrender.com", "/uploads": "https://mern-quickkart-agentic-ai-1.onrender.com" } },
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": "http://localhost:5000",
+      // "/uploads" proxy hataya hai taaki conflict na ho
+    }
+  }
 });
